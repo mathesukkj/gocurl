acc085e2f1641fd1d7cb3f2be6d0ab5f45f24682
Matheus Silva <matheus.silva@in8.com.br>
2024-03-29 18:22:50 -0300

    feat: simple get requests with http package

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,39 @@
 /*
 Copyright © 2024 Matheus Kemuel kemuel.g7363@gmail.com
-
 */
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gocurl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A golang version of curl",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			log.Fatal("error: no url provided")
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		urlStr := args[0]
+		res, err := http.Get(urlStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		r, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(r))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,15 +46,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gocurl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
